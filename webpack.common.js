9752f5a5eccdeec841308ee28db7de7ecdcc24b6
pijush barik <pijush.barik8@gmail.com>
2020-01-19 18:26:37 +0530

    update: changed all images to webp format

--- webpack.common.js
+++ webpack.common.js
@@ -31,7 +31,7 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-                test: /\.(jpg|png)$/,
+                test: /\.(jpg|png|webp)$/,
                 use: [
                 {
                     loader: 'file-loader',
