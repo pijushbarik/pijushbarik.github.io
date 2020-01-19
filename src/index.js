7e23c0c816db79825bdf3ce8863e770c79034248
pijush barik <pijush.barik8@gmail.com>
2020-01-19 22:22:16 +0530

    update: removed jquery dependency
    -- removed jquery dependency
    -- implemented custom fade out method
    -- smaller bundle.js code size

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import './scss/style.scss';
-import $ from "jquery";
 
-$(window).on("load", ()  => {
-    $("#body-loader").fadeOut(() => {
-        $("body").css("overflow", "auto");
+if(document.readyState === "complete") {
+    documentOnReady();
+}
+
+let interval = setInterval(() => {
+    if(document.readyState === "complete") {
+        clearInterval(interval);
+        documentOnReady();
+    }    
+}, 100);
+
+const documentOnReady = () => {
+    removeBodyLoader();
+};
+
+const removeBodyLoader = () => {
+    fadeOut("#body-loader", () => {
+        document.querySelector("body").style.overflow = "auto";
     });
-});
+};
+
+const fadeOut = (selector, cb) => {
+    let el = document.querySelector(selector);
+    let opacity = 1;    // initial opacity
+    let timer = setInterval(() => {
+        if(opacity <= 0.1) {
+            clearInterval(timer);
+            el.style.display = "none";
+            cb();
+        }
+        el.style.opacity = opacity;
+        opacity -= opacity * 0.2;
+    }, 35);
+};
