a7345dd5b8b40f98f71f24aebb57248e48f0dc03
Pijush Barik <40261261+pijushbarik@users.noreply.github.com>
2020-01-19 18:36:29 +0530

    Revert "updat: webpack build config & image formats changed to webp"

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+
+
+module.exports = {
+  entry: {
+    main: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader'
+      }
+    ]
+  },
+  devServer: {
+    watchContentBase: true,
+    publicPath: '/build/'
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new CleanWebpackPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer()
+        ]
+      }
+    })
+  ]
+};
