bc8d86047c2d3b9e98241e409df22b30b88de7f3
Pijush Barik <pijush.barik8@gmail.com>
2020-05-12 01:27:07 +0530

    👌 IMPROVE: removed console logs from theme context and set dark as default theme

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,32 +1,30 @@
 import React from "react"
 
 const defaultState = {
-  isDarkMode: false,
+  isDarkMode: true,
   toggleDark: () => {},
 }
 
 const ThemeContext = React.createContext(defaultState)
 
 const ThemeProvider = props => {
-  const [isDarkMode, setDarkMode] = React.useState(false)
+  const [isDarkMode, setDarkMode] = React.useState(true)
 
   const toggleDark = () => {
-    console.log("i have been called")
     const darkMode = !isDarkMode
     // Save to local storage for future requirements
     localStorage.setItem("dark_mode", JSON.stringify(darkMode))
     setDarkMode(darkMode)
-    console.log("changed mode to dark", isDarkMode)
   }
 
   React.useEffect(() => {
-    // Dar mode setting from local storage
+    // Dark mode setting from local storage
     const isDark = JSON.parse(localStorage.getItem("dark_mode"))
 
-    if (isDark) {
+    if (isDark !== null) {
       setDarkMode(isDark)
     } else {
-      setDarkMode(false)
+      setDarkMode(true)
     }
   }, [])
 
