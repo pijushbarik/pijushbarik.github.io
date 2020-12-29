c56e65f79d183c72f2e149b6afa86868f53a3b43
Pijush Barik <pijush.barik8@gmail.com>
2020-12-29 22:16:01 +0530

    ‼️ BREAKING: update all packages - update packages - add taiwind config to purge unused styles

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,35 +2,27 @@ import { useEffect } from "react"
 import useLocalStorage from "./useLocalStorage"
 
 const useDarkMode = () => {
-  // Use our useLocalStorage hook to persist state through a page refresh.
-  // Read the recipe for this hook to learn more: usehooks.com/useLocalStorage
-  const [enabledState, setEnabledState] = useLocalStorage("dark_mode")
+  const [enabledState, setEnabledState] = useLocalStorage("dark_mode", true)
 
-  // If enabledState is defined use it, otherwise fallback to true.
   const enabled = typeof enabledState !== "undefined" ? enabledState : true
 
-  // Fire off effect that add/removes dark mode class
-  useEffect(
-    () => {
-      const element = window.document.body
-      if (enabled) {
-        if (element.classList.contains("light")) {
-          element.classList.replace("light", "dark")
-        } else {
-          element.classList.add("dark")
-        }
+  useEffect(() => {
+    const element = window.document.body
+    if (enabled) {
+      if (element.classList.contains("light")) {
+        element.classList.replace("light", "dark")
       } else {
-        if (element.classList.contains("dark")) {
-          element.classList.replace("dark", "light")
-        } else {
-          element.classList.add("light")
-        }
+        element.classList.add("dark")
       }
-    },
-    [enabled] // Only re-call effect when value changes
-  )
+    } else {
+      if (element.classList.contains("dark")) {
+        element.classList.replace("dark", "light")
+      } else {
+        element.classList.add("light")
+      }
+    }
+  }, [enabled])
 
-  // Return enabled state and setter
   return [enabled, setEnabledState]
 }
 
