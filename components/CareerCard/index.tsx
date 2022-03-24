690a3330b7e705b275eb13b433d3f371d529bdea
Pijush Barik <pijush.barik8@gmail.com>
2022-03-24 23:26:22 +0530

    content change and minor ui updates

---
+++ components/CareerCard/index.tsx
@@ -0,0 +1,22 @@
+type CareerCardProps = {
+  title: string;
+  comapny: string;
+  jobDescription: string;
+  period: string;
+};
+
+const CareerCard: React.FC<CareerCardProps> = (props) => {
+  return (
+    <>
+      <h4 className="text-4xl font-light text-yellow-600">{props.title}</h4>
+
+      <div className="flex flex-col space-y-4">
+        <span className="font-semibold text-xl">{props.comapny}</span>
+        <span className="text-sm text-gray-400">{props.period}</span>
+        <span className="leading-tight max-w-5xl">{props.jobDescription}</span>
+      </div>
+    </>
+  );
+};
+
+export default CareerCard;
