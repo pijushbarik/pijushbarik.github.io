e7ce3cda78409ed237f304d7ab83c59faa2e870b
Pijush Barik <pijush.barik8@gmail.com>
2025-11-22 22:29:28 +0530

    add initial code

---
+++ README.md
@@ -0,0 +1,9 @@
+Astro codebase for https://pijushbarik.com.
+
+```sh
+# run dev server
+pnpm run dev
+
+# run dev server, expose to local network instead of localhost only
+pnpm run dev --host
+```
---
+++ src/content.config.js
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const posts = defineCollection({
+	loader: glob({ pattern: "*.md", base: "./posts" }),
+	schema: z.object({
+		title: z.string(),
+		slug: z.string(),
+		publishDate: z.union([z.string(), z.date()]),
+		description: z.string(),
+		tags: z.array(z.string()),
+	}),
+});
+
+export const collections = { posts };
